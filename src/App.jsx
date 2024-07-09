30dfa70c03bf32d400aa5e35cfb0db967607bc94
Larkin Campbell <Larkincampbell24@gmail.com>
2024-07-09 16:05:54 -0700

    fixex map issue and implementing mobile nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
 import Header from "./components/Header";
 
 const App = () => {
