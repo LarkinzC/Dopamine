18b05ec495936150c141b8a0280ffdb32176e422
Larkin Campbell <Larkincampbell24@gmail.com>
2024-07-11 15:32:32 -0700

    implementing robot background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,22 @@
 import curve from "../assets/hero/curve.png";
 import Section from "./Section";
 import Button from "./Button";
-import robot from "../assets/hero/robot.jpg";
-
+import {robot, heroBackground} from '../assets/'
 const Hero = () => {
   return (
     <Section
-      className="pt-[12rem] -mt-[5.25]"
+      className="pt-[12rem] -mt-[5.25rem]"
       crosses
-      crossesOffSet={"lg:translate-y-[5.25rem]"}
+      crossesOffSet="lg:translate-y-[5.25rem]"
       customPadding
       id="hero"
     >
       <div className="container relative">
         <div
           className="relative z-1 max-w-[62rem]
-            mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]"
-        >
+            mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with Brainwave
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -26,7 +24,7 @@ const Hero = () => {
                 className="absolute top-full left-0 w-full xl:-mt-2"
                 width={624}
                 height={28}
-                alt="curve"
+                alt="Curve"
               />
             </span>
           </h1>
@@ -42,25 +40,31 @@ const Hero = () => {
           </Button>
         </div>
         <div
-          className="relative max-w-[23rem] mx-auto
-            md:max-w-5xl xl:mb-24"
-        >
+          className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
           <div
-            className="relative z-1 p-0.5 rounded-2xl 
-                bg-conic-gradient"
-          >
+            className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
-              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.8rem]" />
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                 <img
                   src={robot}
-                  className="w-full"
-                  width={1440}
-                  height={1800}
-                  alt="hero"
+                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  alt="AI"
+                  width={1024}
+                  height={490}
                 />
               </div>
             </div>
+          </div>
+          <div
+            className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1440}
+              height={1800}
+              alt="Hero Background"
+            />
           </div>
         </div>
       </div>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -21,9 +21,7 @@ const Section = ({
       <div className="hidden absolute top-0 left-5 width-0.25
        h-full bg-stroke-1 pointer-events-none md:block 
        lg:left-7.5 xl:left-10 "/>
-       <div className="hidden absolute top-0 left-5 width-0.25
-       h-full bg-stroke-1 pointer-events-none md:block 
-       lg:right-7.5 xl:right-10 "/>
+       <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10 "/>
        {crosses && (
          <>
          <div className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25
