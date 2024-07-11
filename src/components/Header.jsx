e50d30102cfa6fd73bd11d49fa1587637700795e
Larkin Campbell <Larkincampbell24@gmail.com>
2024-07-10 21:39:39 -0700

    building section componenet and starting hero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import Button from "./Button";
 import MenuSvg from '../assets/svg/MenuSvg'
 import { HamburgerMenu } from "./design/Header";
 import { useState } from 'react'
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 const Header = () => {
   const pathname = useLocation();
@@ -13,12 +14,16 @@ const Header = () => {
   const toggleNavigation = () => {
     if(openNavigation) {
         setOpenNavigation(false)
+        enablePageScroll()
     } else {
         setOpenNavigation(true)
+        disablePageScroll()
     }
   }
 
   const handleClick = () => {
+    if(!openNavigation) return 
+    enablePageScroll()
     setOpenNavigation(false)
   }
 
