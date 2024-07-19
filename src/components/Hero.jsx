c3d45bceaf259b225b6a73eeb721f47ecedaa83c
Larkin Campbell <Larkincampbell24@gmail.com>
2024-07-19 13:36:19 -0700

    bento boxing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -53,14 +53,14 @@ const Hero = () => {
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
-              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+              <div className="h-[1.4rem] bg-n-9 rounded-t-[0.9rem]" />
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                 <img
                   src={robot}
                   className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
                   alt="AI"
                   width={1024}
-                  height={490}
+                  height={480}
                 />
                   <Generating className='absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8
                   md:w-[31rem] md:-translate-x-1/2'/>
